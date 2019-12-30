f2100d5da9b2ffde992a45f03b46a543866a1fc9
usayuki <masapp.01a@gmail.com>
2019-12-30 19:47:01 +0900

    fragment対応

--- app/src/main/java/org/usayuki/photogallerysample/MainActivity.kt
+++ app/src/main/java/org/usayuki/photogallerysample/MainActivity.kt
@@ -1,35 +1,19 @@
 package org.usayuki.photogallerysample
 
-import android.app.ActivityOptions
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Pair
-import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val GRID_SPAN = 3
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById(R.id.recycler_thumbnail) as RecyclerView
-        recyclerView.layoutManager = GridLayoutManager(this, GRID_SPAN)
-        recyclerView.adapter = RecyclerViewAdapter(object : RecyclerViewAdapter.ListListener {
-            override fun onClickItem(view: View, resource: Int) {
-                val intent = Intent(this@MainActivity, PreviewActivity::class.java)
-                intent.putExtra("image", resource)
-                val options = ActivityOptions.makeSceneTransitionAnimation(this@MainActivity, Pair.create(view, "preview"))
-                startActivity(intent, options.toBundle())
-            }
-        })
-        recyclerView.setHasFixedSize(true)
+        if (savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.container, MainFragment.newInstance())
+            transaction.commit()
+        }
     }
 
 
---
+++ app/src/main/java/org/usayuki/photogallerysample/MainFragment.kt
@@ -0,0 +1,42 @@
+package org.usayuki.photogallerysample
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.util.Pair
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainFragment : Fragment() {
+
+    companion object {
+        private const val GRID_SPAN = 3
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById(R.id.recycler_thumbnail) as RecyclerView
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), GRID_SPAN)
+        recyclerView.adapter = RecyclerViewAdapter(object : RecyclerViewAdapter.ListListener {
+            override fun onClickItem(view: View, resource: Int) {
+                val intent = Intent(requireActivity(), PreviewActivity::class.java)
+                intent.putExtra("image", resource)
+                val options = ActivityOptions.makeSceneTransitionAnimation(requireActivity(), Pair.create(view, "preview"))
+                startActivity(intent, options.toBundle())
+            }
+        })
+        recyclerView.setHasFixedSize(true)
+    }
+}
